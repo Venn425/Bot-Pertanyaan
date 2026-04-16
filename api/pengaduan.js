df31b5c408331bd46527b47a269db5ac412133ac
Venn425 <stevenmoleg@gmail.com>
2026-04-16 14:45:21 +0700

    bot

--- api/pengaduan.js
+++ api/pengaduan.js
@@ -1,14 +1,28 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === "POST") {
+        const { nama, email, pesan } = req.body;
+
+        const message = `
+📩 Pertanyaan Baru:
+👤 Nama: ${nama}
+📧 Email: ${email}
+💬 Pesan: ${pesan}
+        `;
+
+        await fetch(`https://api.telegram.org/bot8761048983:AAHDX6MXBGFOm7R_eXLXrMnzVTQCgifJ4Lg/sendMessage`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                chat_id: "7955039754",
+                text: message
+            })
+        });
+
         return res.status(200).json({
             success: true,
-            message: "Pesan berhasil dikirim!"
-        });
-    }
-
-    if (req.method === "GET") {
-        return res.status(200).json({
-            message: "API aktif"
+            message: "Pesan terkirim ke Telegram!"
         });
     }
 
