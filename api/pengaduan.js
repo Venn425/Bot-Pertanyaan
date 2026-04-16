6267ddbb36ff1834d3d1a8b0fed5c4f9b43707ce
Venn425 <stevenmoleg@gmail.com>
2026-04-16 14:40:30 +0700

    bot

--- api/pengaduan.js
+++ api/pengaduan.js
@@ -1,21 +1,18 @@
 export default function handler(req, res) {
     if (req.method === "POST") {
-        const { nama, email, pesan } = req.body;
-
         return res.status(200).json({
             success: true,
             message: "Pesan berhasil dikirim!"
         });
     }
 
-    // HANDLE GET
     if (req.method === "GET") {
         return res.status(200).json({
             message: "API aktif"
         });
     }
 
-    res.status(405).json({
+    return res.status(405).json({
         message: "Method tidak diizinkan"
     });
 }
